f4022e8bb36cabb313bf61d21d8747d61650d5df
Andrés Mora <andres@quimify.com>
2026-06-19 08:21:10 -0500

    add structured request logging with pino

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import pinoHttp from "pino-http";
 import { productsRouter } from "./modules/products/products.routes.js";
+import { logger } from "./lib/logger.js";
 
 export const app = express();
 
+app.use(pinoHttp({ logger }));
 app.use(express.json());
 app.use("/products", productsRouter);
 
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { app } from "./app.js";
 import { env } from "./config/env.js";
+import { logger } from "./lib/logger.js";
 
 app.listen(env.PORT, () => {
-  console.log(`API listening on http://localhost:${env.PORT}`);
+  logger.info(`API listening on http://localhost:${env.PORT}`);
 });
